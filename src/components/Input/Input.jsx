75cd39bbd1a6abe752aa953ef741917b61f49776
mengiefen <mengiefen@gmail.com>
2022-03-15 22:39:34 +0300

    update calculator display

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,21 +3,40 @@ import PropTypes from 'prop-types';
 
 class Input extends PureComponent {
   render() {
-    const { className, value } = this.props;
+    const {
+      className,
+      value,
+      firstArgument,
+      operator,
+      secondArgument,
+    } = this.props;
     return (
-      <input
-        type="text"
-        className={className}
-        disabled
-        value={value}
-      />
+      <div className="display">
+        <p className="display-top">
+          {firstArgument}
+          {operator}
+          {secondArgument}
+        </p>
+        <p className={className}>
+          {value}
+        </p>
+      </div>
     );
   }
 }
 
+Input.defaultProps = {
+  firstArgument: '',
+  secondArgument: '',
+  operator: '',
+};
+
 Input.propTypes = {
   value: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
+  firstArgument: PropTypes.string,
+  secondArgument: PropTypes.string,
+  operator: PropTypes.string,
 };
 
 export default Input;
