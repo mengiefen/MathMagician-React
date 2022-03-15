d7120973dd8d180301c885eed7f29b624c573e80
mengiefen <mengiefen@gmail.com>
2022-03-15 22:42:30 +0300

    update calcualtor react component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,6 +2,9 @@ import React, { PureComponent } from 'react';
 import Container from './Container/Container';
 import Input from './Input/Input';
 import Buttons from './Button/Buttons';
+import Operate from '../Logic/operate';
+
+const operate = new Operate();
 
 const btnItems = [
   'AC', '+/-', '%', '/',
@@ -14,23 +17,100 @@ const btnItems = [
 class Calculator extends PureComponent {
   constructor(props) {
     super(props);
+    this.firstArgument = '';
+    this.secondArgument = '';
+    this.operator = '';
     this.state = {
       displayValue: '',
     };
-    this.triggerClick = this.triggerClick.bind(this);
+    this.display = this.display.bind(this);
   }
 
-  triggerClick(value) {
-    this.setState((state) => ({
-      displayValue: state.displayValue + value,
-    }));
+  display(value) {
+    const { displayValue } = this.state;
+    if (!this.firstArgument && operate.checkForOperator(value)) return;
+    if (displayValue
+        && !this.firstArgument
+        && !this.secondArgument
+        && !this.operator
+        && operate.checkForNumber(value)) {
+      this.clearDisplay();
+    }
+
+    if (this.firstArgument && operate.checkForOperator(value)) {
+      this.clearDisplay();
+      this.operator = value;
+      this.setState({
+        displayValue: value,
+      });
+    }
+    if (
+      !this.operator
+      && !operate.checkForOperator(value)
+      && operate.checkForNumber(value)
+      && operate.checkForDots(this.firstArgument, value)
+    ) {
+      this.firstArgument += value;
+      this.setState((state) => ({
+        displayValue: state.displayValue + value,
+      }));
+    }
+    if (
+      this.firstArgument
+      && this.operator
+      && operate.checkForNumber(value)
+      && operate.checkForDots(this.secondArgument, value)
+    ) {
+      this.secondArgument += value;
+      this.setState(() => ({
+        displayValue: this.secondArgument,
+      }));
+    }
+
+    if (value === 'AC') {
+      this.clearDisplay();
+      this.clearAguments();
+    }
+
+    if ((this.firstArgument && this.operator && this.secondArgument)
+      && (value === '=')) {
+      this.clearDisplay();
+      const result = operate.compute(
+        this.operator,
+        Number(this.firstArgument),
+        Number(this.secondArgument),
+      );
+      this.clearAguments();
+
+      this.setState({
+        displayValue: String(result),
+      });
+    }
+  }
+
+  clearDisplay() {
+    this.setState({
+      displayValue: '',
+    });
+  }
+
+  clearAguments() {
+    this.firstArgument = '';
+    this.secondArgument = '';
+    this.operator = '';
   }
 
   render() {
     const { displayValue } = this.state;
     return (
       <div className="container">
-        <Input value={displayValue} className="calc-display" />
+        <Input
+          value={displayValue}
+          className="calc-display"
+          firstArgument={this.firstArgument}
+          secondArgument={this.secondArgument}
+          operator={this.operator}
+        />
         <Container>
           <>
             {btnItems.flat().map((btn) => (
@@ -38,7 +118,7 @@ class Calculator extends PureComponent {
                 className={btn === '0' ? 'btn long' : 'btn'}
                 key={btn}
                 value={btn}
-                onClick={this.triggerClick}
+                onClick={this.display}
               />
             ))}
           </>
