c0f615db2dce373846c4e901ea812676965ffe62
mengiefen <mengiefen@gmail.com>
2022-03-16 16:52:28 +0300

    add timer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import Time from './components/Time/Time';
 
 const App = () => (
   <div className="App">
+    <Time />
     <Calculator />
   </div>
 );
