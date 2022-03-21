5d4852b397c7e79cae6277e7dd92ae18a7cb0393
mengiefen <mengiefen@gmail.com>
2022-03-22 01:21:55 +0300

    remove comments from Qoutes.jsx file

--- src/Pages/Qoutes.jsx
+++ src/Pages/Qoutes.jsx
@@ -2,21 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 const Qoutes = () => {
   const [qoute, setQoute] = useState('');
-  // const [source, setSource] = useState('');
   useEffect(async () => {
-    const index = Math.floor(Math.random() * 15);
     const response = await fetch('https://dummyjson.com/quotes/random');
     const result = await response.json();
-    const image = await fetch('https://api.pexels.com/v1/search?query=nature');
-    const imgResult = await image.json();
-    // setSource(imgResult.photos[index].src.large);
-    console.log(imgResult.photos[index].src.large);
     setQoute(result.quote);
   }, []);
-
-  // const background = {
-  //   backgroundImage: `${source}`,
-  // };
 
   return (
     <div
