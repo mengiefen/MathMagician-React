6cf728214828d2a26771dfff04c3c3346215a3f1
mengiefen <mengiefen@gmail.com>
2022-03-22 00:40:31 +0300

    update calculator page

---
+++ src/Pages/Calculator.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState, useRef } from 'react';
+import Container from '../components/Container/Container';
+import Display from '../components/Display/Display';
+import Buttons from '../components/Button/Buttons';
+import calculate from '../Logic/calculator';
+
+const btnItems = ['AC', '+/-', '%', '÷',
+  '7', '8', '9', 'x', '4', '5', '6', '-',
+  '1', '2', '3', '+', '0', '.', '=',
+];
+
+function useKey(cb) {
+  const callbackRef = useRef(cb);
+
+  useEffect(() => {
+    callbackRef.current = cb;
+  });
+
+  function handleKeyPress(event) {
+    if (event.code) {
+      callbackRef.current(event);
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('keypress', handleKeyPress);
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, []);
+}
+
+const Calculator = () => {
+  const [total, setTotal] = useState('');
+  const [next, setNext] = useState('');
+  const [operation, setOperation] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
+
+  const display = (result, value) => {
+    setNext(result.next);
+    setTotal(result.total);
+    setOperation(result.operation);
+
+    if ((!total && !operation) || (total && operation)) {
+      setDisplayValue(result.next);
+    }
+
+    if (
+      ((!next && total) || (next && total))
+      && (value === '=' || value === 'Enter')
+    ) {
+      setNext('');
+      setOperation('');
+      setDisplayValue(result.total);
+    }
+    if (value === 'AC' || value === 'q') {
+      setDisplayValue(null);
+      setNext(null);
+      setOperation(null);
+      setTotal(null);
+    }
+
+    if (value === '+/-') {
+      setDisplayValue(String(-displayValue));
+    }
+  };
+
+  const update = (value) => {
+    const result = calculate({ total, next, operation }, value);
+    display(result, value);
+  };
+
+  const handleCallback = (e) => {
+    update(e.key);
+  };
+  useKey(handleCallback);
+
+  return (
+    <div className="calc-page">
+      <h2>Let&apos;s do some math! </h2>
+      <div className="container">
+        <Display
+          value={displayValue}
+          className="calc-display"
+          total={total}
+          next={next}
+          operation={operation}
+        />
+        <Container>
+          <>
+            {btnItems.flat().map((btn) => (
+              <Buttons
+                className={btn === '0' ? 'btn long' : 'btn'}
+                key={btn}
+                value={btn}
+                onClick={(val) => update(val)}
+              />
+            ))}
+          </>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
