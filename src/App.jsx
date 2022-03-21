e3b95c909ef7f4c7fd45d4cf8b0d403155c3bab7
mengiefen <mengiefen@gmail.com>
2022-03-22 00:38:06 +0300

    add routes for the pages

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Calculator from './Pages/Calculator';
+import Header from './components/Header/Header';
+import Home from './Pages/Home';
+import Qoutes from './Pages/Qoutes';
+
+const App = () => (
+  <div className="App">
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+      }}
+    >
+      <Header />
+    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/qoutes" element={<Qoutes />} />
+
+    </Routes>
+  </div>
+);
+
+export default App;
